aff303ca8f197b795e6dd0446de7bc0ac994ebb7
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-07-03 02:12:00 +0300

    updated js display function

--- Flatdango.js
+++ Flatdango.js
@@ -2,10 +2,8 @@
 fetch("http://localhost:3000/films")
   .then(response => response.json())
   .then(data => {
-    data.forEach(film=>{
-        updateDetails(film)
-        populateFilmList()
-
+    data.forEach(movies=>{
+      updateDetails(movies)
     })
 })
   .catch(error => {
@@ -34,7 +32,20 @@ function updateDetails(movie) {
 
 }
 
+function handleFilmSelection(event) {
+    const filmId = event.target.dataset.filmId;
+    const selectedFilm = films.find(film => film.id === filmId);
+    updateDetails(selectedFilm);
+}
 
+//create menu list for each movie
+const filmsList = document.getElementById('films');
+const listItem = document.createElement('li');
+listItem.className = 'film-item';
+listItem.textContent = film.title;
+listItem.dataset.filmId = film.id;
+listItem.addEventListener('click', handleFilmSelection);
+filmsList.appendChild(listItem);
 
 
 
