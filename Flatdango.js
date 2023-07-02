907943c15f35baab3ebfeb0fbe8a64929e148ab9
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-07-03 02:30:41 +0300

    added js display list menu

--- Flatdango.js
+++ Flatdango.js
@@ -32,22 +32,25 @@ function updateDetails(movie) {
 
 }
 
+fetch("http://localhost:3000/films")
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(movie=>{
+      //create menu list for each movie
+      const filmsList = document.getElementById('films');
+      const listItem = document.createElement('li');
+      listItem.className = 'movie-item';
+      listItem.textContent = movie.title;
+      listItem.addEventListener('click', handleFilmSelection);
+      filmsList.appendChild(listItem);
+
+    })
+})
+
 function handleFilmSelection(event) {
     const filmId = event.target.dataset.filmId;
     const selectedFilm = films.find(film => film.id === filmId);
     updateDetails(selectedFilm);
 }
-
-//create menu list for each movie
-const filmsList = document.getElementById('films');
-const listItem = document.createElement('li');
-listItem.className = 'film-item';
-listItem.textContent = film.title;
-listItem.dataset.filmId = film.id;
-listItem.addEventListener('click', handleFilmSelection);
-filmsList.appendChild(listItem);
-
-
-
 
 
